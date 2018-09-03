0fe3cfeb497d37d20fba304e4fcaba5534763728
Pucoroyo <pucoroyo@gmail.com>
2018-09-03 21:30:47 +0700

    make calculator

---
+++ rumus.js
@@ -0,0 +1,24 @@
+ // Luas persegi = p.l
+var panjang = 10;
+var lebar   = 11;
+console.log (panjang * lebar);
+
+// Volume kubus = s.s.s.s / s.4
+var sisi = 4
+console.log (sisi * 4);
+
+// Luas segitiga = 1/2.a.t
+var setengah = 0.5;
+var alas     = 2;
+var tinggi   = 3;
+console.log(setengah * alas * tinggi);
+
+// Luar jajar genjang = a.t
+var alasjajar = 10;
+var tinggijajar = 11;
+console.log(alasjajar * tinggijajar);
+
+// Luas permukaan tabung = 2.luas alas + luas selimut 
+var luasalas = 4;
+var luasselimut = 7;
+console.log(2 * luasalas * luasselimut );
